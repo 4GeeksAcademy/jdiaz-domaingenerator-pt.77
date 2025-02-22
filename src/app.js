95e7c2bd2ae6f1ee17325975e8c9814c1546c3be
jdiaz1993 <diazjoseph366@gmail.com>
2025-02-22 00:30:03 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  let pronoun = ["the", "we", "her", "them", "us"]
+  let adjective = ["small", "big", "ugly", "pretty", "loud"]
+  let noun = ["dog", "park", "boat", "car", "baseball"]
+  let extension = [".com", ".net", ".gov", ".org", ".edu"]
+
+  for(let i = 0; i < pronoun.length; i++){
+    for(let a = 0; a < adjective.length; a++) {
+      for(let b = 0; b < noun.length; b++){
+        for(let c = 0; c < extension.length; c++){
+        console.log(pronoun[i] + adjective[a] + noun[b] + extension[c])
+        }
+      }
+    }
+  }
+
 };
